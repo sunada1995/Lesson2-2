f6dab25aa63a44cbcde0b624234825a6ae19aabb
sunada1995 <sunada.takanari1995@email.com>
2025-05-02 17:46:49 +0900

    20250417

--- src/main/java/com/seiken_soft/controller/EmployeeMainteController.java
+++ src/main/java/com/seiken_soft/controller/EmployeeMainteController.java
@@ -1,18 +1,25 @@
 package com.seiken_soft.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EmployeeMainteController {
 
 	@GetMapping("/mainte")
-	public ModelAndView viewNextPage() {
+	public ModelAndView viewNextPage(@RequestParam ("shainName") String shainId) {
+		shainId(shainId);
+		
 		ModelAndView model = new ModelAndView();
 		model.setViewName("mainteEmployee");
 		return model;
 	}
 	
 
+	public String shainId(Model model,String shainId) {
+		
+	}
 }
